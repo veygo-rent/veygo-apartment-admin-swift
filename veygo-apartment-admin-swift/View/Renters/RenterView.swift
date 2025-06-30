fea83e5ba84f483a7fde6ad55399c9aba21301f1
Shenghong <me@zhoud2001.io>
2025-06-30 03:09:04 -0400

    hmmm

--- veygo-apartment-admin-swift/View/Renters/RenterView.swift
+++ veygo-apartment-admin-swift/View/Renters/RenterView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 public struct RenterView: View {
+    struct Person: Identifiable {
+        let givenName: String
+        let familyName: String
+        let emailAddress: String
+        let id = UUID()
+
+
+        var fullName: String { givenName + " " + familyName }
+    }
+
+
+    @State private var people = [
+        Person(givenName: "Juan", familyName: "Chavez", emailAddress: "juanchavez@icloud.com"),
+        Person(givenName: "Mei", familyName: "Chen", emailAddress: "meichen@icloud.com"),
+        Person(givenName: "Tom", familyName: "Clark", emailAddress: "tomclark@icloud.com"),
+        Person(givenName: "Gita", familyName: "Kumar", emailAddress: "gitakumar@icloud.com")
+    ]
+
     public var body: some View {
-        Text("RenterView")
+        Table(people) {
+            TableColumn("Given Name", value: \.givenName)
+            TableColumn("Family Name", value: \.familyName)
+            TableColumn("E-Mail Address", value: \.emailAddress)
+        }
     }
 }
 
