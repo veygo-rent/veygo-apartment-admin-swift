00725514bd59ec2d99ed8f37cbd389b45585b7ce
Shenghong <me@zhoud2001.io>
2025-07-07 00:47:31 -0400

    delete unnecessary code

--- veygo-apartment-admin-swift-tests/veygo_apartment_admin_swift_tests.swift
+++ veygo-apartment-admin-swift-tests/veygo_apartment_admin_swift_tests.swift
@@ -8,9 +8,6 @@
 import Testing
 
 struct veygo_apartment_admin_swift_tests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
+    
 
 }
