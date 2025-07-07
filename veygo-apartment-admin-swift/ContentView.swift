ab0e4ea9ad5bba95dbafea62ac2fb133bbcd9e2c
Shenghong <me@zhoud2001.io>
2025-07-06 22:28:42 -0400

    Initial Commit

---
+++ veygo-apartment-admin-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  veygo-apartment-admin-swift
+//
+//  Created by Shenghong Zhou on 7/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ veygo-apartment-admin-swift/veygo_apartment_admin_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  veygo_apartment_admin_swiftApp.swift
+//  veygo-apartment-admin-swift
+//
+//  Created by Shenghong Zhou on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct veygo_apartment_admin_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
